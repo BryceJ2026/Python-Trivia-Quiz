29aa15dd3d964df0a9fc48ab442c86ca4e4a50c9
Bryce? <bryce.jones2026@gbwl.org>
2025-10-03 13:40:36 +0000

    started questions

--- game.py
+++ game.py
@@ -1,6 +1,56 @@
+trivia = [
+    {
+        'question':'question body'
+        'answer': 'correct',
+        'options': ['option a', 'option b', 'correct']
+    },
+    {
+        'question': 'How many hours does it take to beat Terraria?'
+        'answer': '230',
+        options: ['100', '60', '230']
+
+    },
+
+    {
+        'question': 'What mob in minecraft is unused, but still in the game?'
+        'answer': 'Giant',
+        options: ['Iceologer', 'Tuffle Golem', 'Giant']
+
+    },
+    {
+        'question': 'What joker in balatro is the best?'
+        'answer': 'None',
+        options: ['Blueprint', 'Yorick', 'None']
+
+    },
+    {
+        'question': 'What is the maximum amount of torches you can carry in one slot in Darkest Dungeon?'
+        'answer': '8',
+        options: ['16', '4', '8']
+
+    },
+    {
+        'question': 'Can Radann be parried in Elden Ring?'
+        'answer': 'No',
+        options: ['Maybe', 'Yes', 'No']
+
+    },
+    {
+        'question': 'Why did Radann learn gravity magic?'
+        'answer': 'To ride his horse',
+        options: ['He did not', 'To be cool', 'To ride his horse']
+
+    },
+]
+
+
+# Define functions to ask the questions and compute the score
+# GAMER LOOP, call your functions below
 def main():
     print("Starting a new Trivia game...")
-    user_name = input("Enter your name: ")
+    user_name = input("Enter your name already. I don't have time for this: ")
     print(f"Welcome, {user_name}!")
+
+
 
 main()
